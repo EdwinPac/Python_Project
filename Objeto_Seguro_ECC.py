3898f41d2fceaf22547314f695feef5c277909e7
EdwinPac <epachecoq1512@gmail.com>
2021-12-11 00:00:30 -0600

    v1.1

--- Objeto_Seguro_ECC.py
+++ Objeto_Seguro_ECC.py
@@ -7,7 +7,8 @@ logging.basicConfig(level=logging.DEBUG, format="\n%(asctime)s - %(message)s")
 
 
 class ObjetoSeguro:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.__private_key = generate_eth_key()
         self.__private_key_hex = self.__private_key.to_hex()
         print(self.__private_key_hex)
@@ -22,8 +23,12 @@ class ObjetoSeguro:
         b85_decode_message = b85decode(message_to_bytes)
         return b85_decode_message
 
-    @staticmethod
-    def saludar(plain_text: str, publickey: str) -> str:
+    def __base85_encode(self, message: str) -> bytes:
+        message_to_bytes = message.encode("utf-8")
+        b85_encode_message = b85encode(message_to_bytes)
+        return b85_encode_message
+
+    def saludar(self, plain_text: str, publickey: str) -> str:
         plain_text_to_bytes = plain_text.encode("utf-8")
         message_cypher = encrypt(publickey, plain_text_to_bytes)
         b85_cypher = b85encode(message_cypher).decode("utf-8")
@@ -35,24 +40,19 @@ class ObjetoSeguro:
         message_decrypted_string = message_decrypted.decode("utf-8")
         return message_decrypted_string
 
-
-logging.debug("**********Generando Llaves Públicas**********")
-edwin = ObjetoSeguro()
-pubkey_ed = edwin.key_pub
-print("Llave pública de Edwin en Hexadecimal\n", pubkey_ed)
-karen = ObjetoSeguro()
-pubkey_ka = karen.key_pub
-print("Llave pública de Karen en Hexadecimal\n", pubkey_ed)
-msj_ed = "Hola mi nombre es Edwin, ¿Cómo estás?"
+    def guardar_msj(self, plain_text: str) -> str:
+        python_project = open("archivo.txt", "a")
+        python_project.write(plain_text)
+        python_project.close()
 
 
-logging.debug("**********Cifrando un mensaje********** ")
-msj_cif = edwin.saludar(msj_ed, pubkey_ka)
-print("Cifrando Mensaje")
-print("Mensaje Cifrado", msj_cif)
 
 
-logging.debug("**********Descifrando Mensaje**********")
-msj_dcif = karen.responder(msj_cif)
-print("El mensaje es:", msj_dcif)
+
+
+
+
+
+
+
 
